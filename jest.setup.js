4860f6c0f7084e180e097ff832c1e1330c590978
Emma-em0609 <emma.em207@gmail.com>
2025-02-26 13:19:09 +0200

    Add Jest configuration, test setup, and initial test cases for authentication and validation

---
+++ jest.setup.js
@@ -0,0 +1,24 @@
+require('@testing-library/jest-dom');
+
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+const mockRouter = {
+    push: jest.fn(),
+    prefetch: jest.fn(),
+    query: {}
+};
+
+jest.mock('next/router', () => ({
+    useRouter: () => mockRouter
+}));
+
+Object.defineProperty(window, 'localStorage', {
+    value: {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        clear: jest.fn(),
+        removeItem: jest.fn(),
+    },
+}); 
